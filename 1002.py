99db718a324c00993dc76e58f3cebdc83be21ec3
Yuri Freire <yuriifreire@hotmail.com>
2018-08-22 20:19:27 -0300

    intialsTest
    first 5 initials problems

---
+++ 1002.py
@@ -0,0 +1,7 @@
+n = 3.14159
+
+r = float(input())
+
+area = (r*r) * n
+
+print('A=%.4f' %area)
---
+++ 1005.py
@@ -0,0 +1,6 @@
+n1 = float(input())
+n2 = float(input())
+
+MEDIA = (((n1 * 3.5) + (n2 * 7.5)) / 11)
+
+print("MEDIA = %0.5f" %MEDIA)
